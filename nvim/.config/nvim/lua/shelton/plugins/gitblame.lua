492f911db228c82f696317e23b88897068244270
Shelton Fr <sheltonfjs@gmail.com>
2024-07-03 20:20:19 +0200

    chore: clean env

--- nvim/.config/nvim/lua/shelton/plugins/gitblame.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'f-person/git-blame.nvim',
-  event = 'VeryLazy',
-  opts = {
-    enabled = false, -- disable by default, enabled only on keymap
-    date_format = '%m/%d/%y %H:%M:%S', -- more concise date format
-  }
-}
